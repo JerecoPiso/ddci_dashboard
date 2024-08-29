b372640511c84fdfb286ff032bd111ac18033823
JerecoPiso <jerecojamespiso@gmail.com>
2024-08-29 16:05:19 +0800

    add toltip to the user add and update client button

--- src/components/datatable/usermanagement/data-table-toolbar.tsx
+++ src/components/datatable/usermanagement/data-table-toolbar.tsx
@@ -4,26 +4,31 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { DataTableViewOptions } from "@/components/datatable/components/data-table-view-options";
 import { PlusCircle, EditIcon } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 interface DataTableToolbarProps<TData> {
   table: Table<TData>;
   addUser: () => void;
-  editClient: () => void
+  editClient: () => void;
 }
 export function DataTableToolbar<TData>({
   table,
   addUser,
-  editClient
+  editClient,
 }: DataTableToolbarProps<TData>) {
   const isFiltered = table.getState().columnFilters.length > 0;
-  const handleAddUser = () =>{
-   addUser()
-  }
-  const handleEditClient = () =>{
-    editClient()
-  }
+  const handleAddUser = () => {
+    addUser();
+  };
+  const handleEditClient = () => {
+    editClient();
+  };
   return (
     <div className="flex items-center justify-between">
-      
       <div className="flex flex-1 items-center space-x-2">
         <Input
           placeholder="Filter lastname..."
@@ -46,9 +51,40 @@ export function DataTableToolbar<TData>({
           </Button>
         )}
       </div>
-      <Button onClick={ handleAddUser}  className="h-[32px] px-3 bg-transparent hover:text-white dark:hover:text-slate-800 dark:text-slate-100 text-slate-800 border rounded-sm mr-1"><PlusCircle className="h-5 w-5" /></Button >
-      <Button onClick={ handleEditClient}  className="h-[32px] px-3 bg-transparent hover:text-white dark:hover:text-slate-800 dark:text-slate-100 text-slate-800 border rounded-sm mr-1"><EditIcon className="h-5 w-5" /></Button >
-    
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            {" "}
+            <Button
+              onClick={handleAddUser}
+              className="h-[32px] px-3 bg-transparent hover:text-white dark:hover:text-slate-800 dark:text-slate-100 text-slate-800 border rounded-sm mr-1"
+            >
+              <PlusCircle className="h-5 w-5" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Add User</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            {" "}
+            <Button
+              onClick={handleEditClient}
+              className="h-[32px] px-3 bg-transparent hover:text-white dark:hover:text-slate-800 dark:text-slate-100 text-slate-800 border rounded-sm mr-1"
+            >
+              <EditIcon className="h-5 w-5" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Update the client of selected user/s</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
       <DataTableViewOptions table={table} />
     </div>
   );
